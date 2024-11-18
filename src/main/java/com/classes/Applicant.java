bc972c50d501c7acdf7c48d2b4606ab7e4bc29e8
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2024-11-18 20:57:04 +0500

    utility class and login working

--- src/main/java/com/classes/Applicant.java
+++ src/main/java/com/classes/Applicant.java
@@ -2,23 +2,11 @@ package com.classes;
 
 import java.util.List;
 
-public class Applicant {
-    private String applicantID;
-    private String name;
-    private String contactInfo;
-    private String applicationStatus;
-    private String applicationDate;
-    private String preferences;
-
-    private List<Notification> notifications;
+public class Applicant extends User {
+  
 
     public Applicant(String applicantID, String name, String contactInfo, String applicationStatus, String applicationDate, String preferences) {
-        this.applicantID = applicantID;
-        this.name = name;
-        this.contactInfo = contactInfo;
-        this.applicationStatus = applicationStatus;
-        this.applicationDate = applicationDate;
-        this.preferences = preferences;
+       super(applicantID, name, contactInfo, applicationStatus, applicationDate, preferences);
     }
 
     public void displayRentals() {
