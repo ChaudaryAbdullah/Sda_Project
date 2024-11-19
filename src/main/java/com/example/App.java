1f92a7ec50c85f2b6cf995496cc32cf59e94b23f
maaz7654 <i222125@nu.edu.pk>
2024-11-19 15:48:51 +0500

    maaz here

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -14,53 +14,51 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 1080, 720);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
+		scene = new Scene(loadFXML("login"), 1080, 720);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-
-
-public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		// String url = "jdbc:mysql://localhost:3306/sda_project";
 		// String username = "root";
 		// String password = "12345678";
 		// System.out.println("Connecting database");
 		// Connection connection = null;
-		// try  {
-		// 	connection = DriverManager.getConnection(url, username, password);
-		// 			System.out.println("Database connected!");
-		// 			Statement s = connection.createStatement();
-					
-		// 			ResultSet rs = s.executeQuery("Select * from user");
-					
-		// 			while(rs.next()) {
-		// 				String id = rs.getString(1);
-		// 				String name = rs.getString("name");
-		// 				System.out.println(id + " " + name);
-		// 			}
-					
-		// 			connection.close();
+		// try {
+		// connection = DriverManager.getConnection(url, username, password);
+		// System.out.println("Database connected!");
+		// Statement s = connection.createStatement();
+
+		// ResultSet rs = s.executeQuery("Select * from user");
+
+		// while(rs.next()) {
+		// String id = rs.getString(1);
+		// String name = rs.getString("name");
+		// System.out.println(id + " " + name);
+		// }
+
+		// connection.close();
 		// }catch(Exception e) {
-		// 	e.printStackTrace();
+		// e.printStackTrace();
 
 		// }
-	
-        launch();
-    }
+
+		launch();
+	}
 
 }
