6ab64b2cff3a2bdb39ac08204a3c4bd7a9f6b804
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2024-11-23 10:46:08 +0500

    working on rentals

--- src/main/java/com/Factories/MealFactory.java
+++ src/main/java/com/Factories/MealFactory.java
@@ -4,6 +4,10 @@ import com.BussinessLogic.classes.Meal;
 
 public class MealFactory {
 
+    private static String generateUniqueID() {
+        return "Meal-" + System.currentTimeMillis();
+    }
+
     public Meal CreateMeal(int id, String name, String description, long price) {
         return new Meal(id, name, description, price);
     }
--- src/main/java/com/Factories/RentalFactory.java
+++ src/main/java/com/Factories/RentalFactory.java
@@ -3,6 +3,10 @@ package com.Factories;
 import com.BussinessLogic.classes.Rental;
 
 public class RentalFactory {
+    private static String generateUniqueID() {
+        return "REN-" + System.currentTimeMillis();
+    }
+
     public Rental cretaRental(int id, String name, String address, String facilities, int totalRooms, int availableRooms){
         return new Rental(id, name, address, facilities, totalRooms, availableRooms);
     }
